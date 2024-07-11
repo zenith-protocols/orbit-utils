d2313235bf07c9c03d6f8d88d11eba3c46fdafd9
Robin <70903912+Robeartt@users.noreply.github.com>
2024-07-11 13:47:08 +0200

    Trying to implement deploy stablecoin

--- src/deploy/deployLogic.ts
+++ src/deploy/deployLogic.ts
@@ -7,6 +7,7 @@ import {
   parseError,
 } from '@blend-capital/blend-sdk';
 import { randomBytes } from 'crypto';
+import {Asset as TreasuryAsset} from '../external/treasury.js';
 import {
   Address,
   Asset,
@@ -19,17 +20,14 @@ import {
 import { AddressBook } from '../utils/address-book.js';
 import { config } from '../utils/env_config.js';
 import {
-  invokeClassicOp,
   signWithKeypair,
   TxParams,
   invokeSorobanOperation,
-  sendTransaction,
 } from '../utils/tx.js';
 import {
   airdropAccount,
   bumpContractCode,
   bumpContractInstance,
-  deployContract,
 } from '../utils/contract.js';
 import { tryDeployStellarAsset } from '../utils/stellar-asset.js';
 import { setupReserve } from '../utils/blend-pool/reserve-setup.js';
@@ -39,9 +37,6 @@ import { TreasuryContract } from '../external/treasury.js';
 
 const mint_amount = BigInt(10_000e7);
 const pool_name = 'OrbitUSD';
-const backstop_take_rate = 0;
-const max_positions = 7;
-const reserves = ['oUSD', 'XLM'];
 
 const txParams: TxParams = {
   account: await config.rpc.getAccount(config.admin.publicKey()),
@@ -69,6 +64,7 @@ export async function initializeOrbit(addressBook: AddressBook, router: string, 
   const treasury = new TreasuryContract(config.treasury);
   const pegkeeper = new PegkeeperContract(config.pegkeeper);
   const bridgeOracle = new BridgeOracleContract(config.bridgeOracle);
+  
   //TODO: Initialize orbit using config.admin
   await invokeSorobanOperation(
     treasury.initialize({
@@ -109,15 +105,20 @@ export async function deployTokenContract(addressBook: AddressBook, name: string
   await bumpContractInstance(name, txParams);
 }
 
-// export async function addStableCoin(addressBook: AddressBook, name: string) {
-//   console.log('Adding stable coin...');
-//   const treasury = new TreasuryContract(addressBook.getContractId("treasury"));
-//   await invokeSorobanOperation(
-//     treasury.addStableCoin(addressBook.getContractId(name)),
-//     PoolContract.parsers.addStableCoin,
-//     txParams
-//   );
-// }
+export async function deployStablecoin(addressBook: AddressBook, token: string, asset: TreasuryAsset, blend_pool: string) {
+  console.log('Deploying new stablecoin...');
+
+  const treasury = new TreasuryContract(config.treasury);
+  await invokeSorobanOperation(
+    treasury.deploy_stablecoin({
+      token: token,
+      asset: asset,
+      blend_pool: blend_pool,
+    }),
+    TreasuryContract.parsers.deploy_stablecoin,
+    txParams
+  );
+}
 
 export async function deployPool(addressBook: AddressBook, name: string, backstop_take_rate: number, max_positions: number) {
   console.log('Deploying pool...');
